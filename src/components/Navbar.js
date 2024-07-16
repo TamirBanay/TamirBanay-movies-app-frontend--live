a4713e225f9adccc185abe422ea0ef24f34f5065
Tamir Banay <banay9329@gmail.com>
2024-07-16 14:59:34 +0300

    DELETE #

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -265,7 +265,7 @@ function ResponsiveAppBar() {
               </div>
             ) : (
               <Button
-                href="/#/login"
+                href="/login"
                 sx={{ my: 2, color: "white", display: "block" }}
               >
                 login
